49fe87d2dd8fa3a276468e76d432c5647988a73c
wanderer <56039464+wanderer-00@users.noreply.github.com>
2024-12-07 21:57:34 +0300

    -

--- HTML/task/script.js
+++ HTML/task/script.js
@@ -3,7 +3,7 @@ let tasks = document.getElementsByClassName('tasks')[0];
 for(i=0; i<task.length; i++){
     tasks.innerHTML += 
     `
-    <div class="task">
+    <div class="task${status(task[i].STATUS)}">
         <div class="work">${task[i].WORK}</div>
         <div class="client">${task[i].CLIENT}</div>
         <div class="product">${task[i].PRODUCT}</div>
@@ -11,6 +11,19 @@ for(i=0; i<task.length; i++){
         <div class="quantity">${task[i].QUANTITY}</div>
         <div class="term">${task[i].TERM}</div>
         <div class="note">${task[i].NOTE}</div>
+        <div class="status">${statusIcon(task[i].STATUS)}</div>
     </div>
     `;
+}
+
+function status(s) {
+    if      (s==0) {return " fire"}
+    else if (s==1) {return " suspended"}
+    else    {return ""}
+}
+
+function statusIcon(si) {
+    if      (s==0) {return "<img src='fire.svg'>"}
+    else if (s==1) {return " suspended"}
+    else    {return ""}
 }
