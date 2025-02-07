3b56b9a99345e312832d9d8a44433b0a76990fab
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-02-07 17:42:47 +0300

    -

--- HTML/task/builderDP.js
+++ HTML/task/builderDP.js
@@ -6,11 +6,20 @@ for(i=0; i<task_DP.length; i++){
     `
     <div class="task${status(task_DP[i].STATUS)}">
         <div class="client">${task_DP[i].CLIENT}</div>
-        <div class="product">${task_DP[i].PRODUCT}</div>
         <div class="article">${task_DP[i].ARTICLE}</div>
-        <div class="quantity">${task_DP[i].QUANTITY} шт.</div>
-        <div class="term">📅 ${task_DP[i].TERM}</div>
+        <div class="article">${task_DP[i].QUANTITY}</div>
+        <div class="article">${task_DP[i].NOTE}</div>
+        <div class="article">${task_DP[i].TERM}</div>
         <div class="status">${statusIcon(task_DP[i].STATUS)}</div>
     </div>
     `;
 }
+
+//    <div class="task${status(task_DP[i].STATUS)}">
+//        <div class="client">${task_DP[i].CLIENT}</div>
+//        <div class="product">${task_DP[i].PRODUCT}</div>
+//        <div class="article">${task_DP[i].ARTICLE}</div>
+//        <div class="quantity">${task_DP[i].QUANTITY} шт.</div>
+//        <div class="term">📅 ${task_DP[i].TERM}</div>
+//        <div class="status">${statusIcon(task_DP[i].STATUS)}</div>
+//    </div>
