251d6319e9a9c0d5814161b73153929d9ad55764
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-04-04 17:29:02 +0300

    -

--- HTML/progress/script.js
+++ HTML/progress/script.js
@@ -6,7 +6,7 @@ for(i=0; i<DTFProgress.length; i++){
 <tr>
     <td>${DTFProgress[i][0]}</td>
     <td><progress max="${DTFProgress[i][2]}" value="${DTFProgress[i][1]}"></progress></td>
-    <td class="a">${DTFProgress[i][1]} / ${DTFProgress[i][2]}</td>
+    <td class="a">${DTFProgress[i][1]}/${DTFProgress[i][2]}</td>
 </tr>
 `
 }
