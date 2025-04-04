f04c26498139eb0a658faafc7cf0b9fbc9b88765
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-04-04 17:27:55 +0300

    -

--- HTML/progress/script.js
+++ HTML/progress/script.js
@@ -1,11 +1,12 @@
 let cluster = document.getElementById('cluster');
 
 for(i=0; i<DTFProgress.length; i++){
-    cluster.innerHTML += `
-        <tr>
-            <td>${DTFProgress[i][0]}</td>
-            <td><progress max="${DTFProgress[i][2]}" value="${DTFProgress[i][1]}"></progress></td>
-            <td class="a">${DTFProgress[i][1]} / ${DTFProgress[i][2]}</td>
-        </tr>
-    `
+    cluster.innerHTML +=
+`
+<tr>
+    <td>${DTFProgress[i][0]}</td>
+    <td><progress max="${DTFProgress[i][2]}" value="${DTFProgress[i][1]}"></progress></td>
+    <td class="a">${DTFProgress[i][1]} / ${DTFProgress[i][2]}</td>
+</tr>
+`
 }
