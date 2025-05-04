23bdc982f421a2940eb2e8cbc2a9fdb860c7eac3
wanderer <56039464+wanderer-00@users.noreply.github.com>
2025-05-05 01:53:50 +0300

    -

--- HTML/monitoring/DTF/script/printHeadStatistics.js
+++ HTML/monitoring/DTF/script/printHeadStatistics.js
@@ -1,18 +1,32 @@
-// код нуждается в оптимизации
 let clusterPHS = document.getElementById('cluster-print-head-stististics');
 
-var time = new Date(); // время сейчас
+let time = new Date(); // время сейчас
 
-var timeSet0 = new Date('12-02-2024'); // дата установки головы #0
-var timeSet1 = new Date('12-18-2024'); // дата установки головы #1
+// голова #0 | причина смерти: просроченный сольвент
+let timeSet0 = new Date('12-02-2024'); // дата установки
+let timeEnd0 = new Date('05-04-2025'); // дата снятия
+// голова #1 | работает
+let timeSet1 = new Date('12-18-2024');
+let timeEnd1 = new Date('05-04-2025');
+// голова #2 | работает
+let timeSet2 = new Date('05-04-2025');
+let timeEnd2 = new Date('??-??-????');
+// голова #3 | планируется ставить
+let timeSet3 = new Date('05-04-2025');
+let timeEnd3 = new Date('??-??-????');
 
-builder(timeSet0, 'WHITE');
-builder(timeSet1, 'CMYK');
+// вызов функции отрисовки блоков голов
+// дата_установки дата_снятия назначение_головы
+builder(timeSet0, timeEnd0, 'WHITE'); // голова #0
+builder(timeSet1, time, 'CMYK'); // голова #1
+builder(timeSet2, time, 'WHITE'); // голова #2
+builder(timeSet3, time, 'CMYK'); // голова #3
 
 // отрисовка виджета
-function builder(days, color){
+function builder(days, time, color){
     // кол-во дней прошедших с установки
-    var days = Math.ceil(Math.abs(days.getTime() - time.getTime()) / (1000 * 3600 * 24));
+//    var days = Math.ceil(Math.abs(days.getTime() - time.getTime()) / (1000 * 3600 * 24));
+    var days = Math.ceil(Math.abs(days.getTime() - Math.abs(time.getTime())) / (1000 * 3600 * 24));
     
     clusterPHS.innerHTML += `
         <div class="print-head-stististics">
